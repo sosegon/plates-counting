0dc1147d2e0e9cbafc25462df4c7f34bd70473c1
Sebastian <anse23@hotmail.com>
2019-04-21 23:01:47 -0500

    refactor: Created class to handle the press moves
    The logic is the same, but for clarity and scalability,
    the processing to count the press moves was moved to a
    new class.

--- counter.py
+++ counter.py
@@ -3,11 +3,11 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 from time import sleep, time
-from scipy.signal import find_peaks
+from press_counter import PressCounter
 
 class Counter:
     """
-    A class to count press moves and plates from a video of a manufacturing
+    A class to count press moves from a video of a manufacturing
     machine.
 
     ...
@@ -19,9 +19,8 @@ class Counter:
 
     Methods
     -------
-    analyse(x_center, half_width, y_center, half_height, y_bar_start,
-    half_bar_width, half_bar_height, tracker_type='BOOSTING')
-        Performs the analysis to count the press moves.
+    analyse(press_counter)
+        Analyses the video to count the number of press moves.
     draw_press_counter(self, outname, frame_numbers)
         Creates a video with text of the press moves.
     """
@@ -32,195 +31,61 @@ class Counter:
         ----------
         filename : str
             Name of the video file to be processed.
-        outname : str
-            Name of the output video.
         """
         self.filename = filename
 
-    def analyse(self,
-                    x_center, half_width,
-                    y_center, half_height,
-                    y_bar_start, half_bar_width, half_bar_height,
-                    tracker_type='BOOSTING', analysis=False):
+
+    def analyse(self, press_counter, analysis=False):
         """
-        Processes the video to count the number of press moves.
-
-        The processing is done by tracking the press. To do so, it is required
-        to define two areas. The main area corresponds to a section of the
-        machine where the tracking will be performed. The second area is
-        contained within the main one. This area corresponds to the object that
-        will be tracked.
-
-        The tracking is performed with a built-in object tracker in OpenCV.
-
-        During the tracking, the position of the top-left corner of the bounding
-        box of the press is recored in every frame. This process creates a set
-        of points. Those points form a sine-shape curve. The peaks of that curve
-        correspond to the lower position of the press. The number of peaks
-        corresponds to the number of press moves.
+        Analyses the video to count the number of press moves.
 
         Parameters
         ----------
-        x_center : int
-            Horizontal position of the center of the main area.
-        half_width : int
-            Half the width of the main area.
-        y_center : int
-            Vertical position of the center of the main area.
-        half_height : int
-            Half the height of the main area.
-        y_bar_start : int
-            Vertical position of the inner area. This value is relative to
-            the main area.
-        half_bar_width : int
-            Half the width of the inner area.
-        half_bar_height : int
-            Half the height of the inner area.
-        tracker_type : str
-            Name of one of the built-in trackers in OpenCV.
+        press_counter : PressCounter
+            Object in charge of count the press moves.
         analysis : bool
             Flag used for analysis during development.
-
-        Returns
-        -------
-        ndarray
-            Indices of the frames where the press is in the bottom position.
         """
 
         cap = cv.VideoCapture(self.filename)
 
-        # Top-left corner of the main area.
-        x_start = int(x_center - half_width)
-        y_start = int(y_center - half_height)
-
-        # Bottom-right corner of the main area.
-        x_end = int(x_center + half_width)
-        y_end = int(y_center + half_height)
-
-        # Dimensions of the main area.
-        width = int(half_width * 2)
-        height = int(half_height * 2)
-
-        # Top-left corner of the inner area, along with y_bar_start. This
-        # position is relative to the main area.
-        x_bar_start = int(half_width - half_bar_width)
-
-        # Bottom-right corner of the inner area. This position is relative to
-        # the main area.
-        x_bar_end = int(x_bar_start + half_bar_width * 2)
-        y_bar_end = int(y_bar_start + half_bar_height * 2)
-
-        # Inner area
-        bbox = (x_bar_start, y_bar_start, half_bar_width * 2, half_bar_height * 2)
-
-        # Tracker
-        (major_ver, minor_ver, subminor_ver) = (cv.__version__).split('.')
-        if int(minor_ver) < 3:
-            tracker = cv.Tracker_create(tracker_type)
-        else:
-            if tracker_type == 'BOOSTING':
-                tracker = cv.TrackerBoosting_create()
-            if tracker_type == 'MIL':
-                tracker = cv.TrackerMIL_create()
-            if tracker_type == 'KCF':
-                tracker = cv.TrackerKCF_create()
-            if tracker_type == 'TLD':
-                tracker = cv.TrackerTLD_create()
-            if tracker_type == 'MEDIANFLOW':
-                tracker = cv.TrackerMedianFlow_create()
-            if tracker_type == 'GOTURN':
-                tracker = cv.TrackerGOTURN_create()
-            if tracker_type == 'MOSSE':
-                tracker = cv.TrackerMOSSE_create()
-            if tracker_type == "CSRT":
-                tracker = cv.TrackerCSRT_create()
-
-        # Read first frame and extact the main area.
+        # Set init frame
         ok, frame = cap.read()
-        frame = frame[y_start:y_end, x_start:x_end, :]
-
-        # Initialize the tracker with the main and inner areas.
-        ok = tracker.init(frame, bbox)
-
-        # Initial vertical position of the inner area.
-        y_pos_history = np.array(int(bbox[1]))
+        press_counter.init_tracker(frame)
 
         if analysis:
-            # Draw bounding box of the tracking object
-            self.__draw_inner_area(
-                frame,
-                int(bbox[0]), int(bbox[1]),
-                int(bbox[2]), int(bbox[3])
-            )
-            sleep(0)
+            press_counter.draw_inner_area(frame)
+            sleep(0.0)
 
         while(1):
-
-            if analysis:
-                # Slow down the movement for better visualization.
-                sleep(0.0)
 
             ok, frame = cap.read()
 
             if not ok:
                 break
 
-            # Extract the main area.
-            frame = frame[y_start:y_end, x_start:x_end, :]
+            press_counter.process_frame(frame)
 
-            # Start timer
-            timer = cv.getTickCount()
-
-            # Find the object defined by the inner area in the first frame.
-            ok, bbox = tracker.update(frame)
-
-            # Frames per second.
-            fps = cv.getTickFrequency() / (cv.getTickCount() - timer);
-
-            if ok:
-                # Vertical position of the tracking object in the current frame.
-                y_pos_history = np.hstack((y_pos_history, int(bbox[1])))
-
-                if analysis:
-                    # Draw bounding box of the tracking object.
-                    self.__draw_inner_area(
-                        frame,
-                        int(bbox[0]), int(bbox[1]),
-                        int(bbox[2]), int(bbox[3])
-                    )
+            if analysis:
+                # Slow down the movement for better visualization.
+                sleep(0.0)
+                press_counter.draw_inner_area(frame)
 
             # Exit if ESC pressed
             k = cv.waitKey(1) & 0xff
             if k == 27:
                 break
 
-        # Normalize the vertical positions of the top-left corner of the
-        # bounding box of the tracking object.
-        min_ = np.min(y_pos_history)
-        max_ = np.max(y_pos_history)
-
-        y_pos_history = y_pos_history - min_
-        y_pos_history = y_pos_history / (max_ - min_)
+        press_counter.calculate_press_down_positions()
+        peaks = press_counter.peaks
 
         if analysis:
-            plt.plot(y_pos_history)
-
-        # Find the peaks of the sine-shape curve.
-        peaks, _ = find_peaks(y_pos_history, distance=15)
-
-        # Filter the peaks to get just those corresponding to the press in the
-        # down position.
-        valid_peaks = np.argwhere(y_pos_history[peaks] > 0.5)
-        peaks = peaks[valid_peaks.ravel()]
-
-        if analysis:
-            plt.plot(peaks, y_pos_history[peaks], 'X')
+            plt.plot(press_counter.y_pos_history)
+            plt.plot(peaks, press_counter.y_pos_history[peaks], 'X');
             plt.savefig('output')
             cv.destroyAllWindows()
 
         cap.release()
-
-        return peaks
 
     def draw_press_counter(self, outname, frame_numbers):
         """
@@ -289,35 +154,6 @@ class Counter:
         out.release()
         cap.release()
 
-    def __draw_inner_area(self, frame, x, y, w, h):
-        """
-        Draws the inner area (bounding box of the tracking object).
-
-        Parameters
-        ----------
-        frame : ndarray
-            3-channel image where the inner area will be drawn.
-        x : int
-            Horizontal position of the top-left corner.
-        y : int
-            Vertical position of the top-left corner.
-        w : int
-            Width of the bounding box.
-        h : int
-            Height of the bounding box.
-
-        """
-        H = frame.shape[0]
-        W = frame.shape[1]
-        p1 = (x, y)
-        p2 = (x + w, y + h)
-        cv.rectangle(frame, p1, p2, (255,0,0), 1, 1)
-
-        # Resize for better visualization
-        frame = cv.resize(frame, (W * 3, H * 3))
-
-        cv.imshow("Tracking", frame)
-
 def main():
     parser = argparse.ArgumentParser(description='plates')
     parser.add_argument('filename', type=str, help='Input video filename')
@@ -368,16 +204,17 @@ def main():
     analysis = args.analysis
 
     counter = Counter(filename)
+    press_counter = PressCounter(xc, hw, yc, hh, yb, bw, bh, tracker_type)
 
     start = time()
-    peaks_indices = counter.analyse(xc, hw, yc, hh, yb, bw, bh, tracker_type, analysis)
+    counter.analyse(press_counter, analysis)
     end = time()
     print("Time to process: {:d}s".format(int(end - start)))
 
     if outname is not None:
         # Draw text to coun the press moves in the video
         start = time()
-        counter.draw_press_counter(outname, peaks_indices)
+        counter.draw_press_counter(outname, press_counter.peaks)
         end = time()
         print("Time to create output video: {:d}s".format(int(end - start)))
 
---
+++ press_counter.py
@@ -0,0 +1,240 @@
+import cv2 as cv
+import numpy as np
+from scipy.signal import find_peaks
+
+class PressCounter:
+    """
+    A class to count press moves from a video frames of a
+    manufacturing machine.
+
+    The processing is done by tracking the press. To do so, it is required
+    to define two areas. The main area corresponds to a section of the
+    machine where the tracking will be performed. The second area (inner area)
+    is contained within the main one. This area corresponds to the object that
+    will be tracked.
+
+    The tracking is performed with a built-in object tracker in OpenCV.
+
+    During the tracking, the position of the top-left corner of the bounding
+    box of the inner area is recorded in every frame. This process creates a set
+    of points. Those points form a sine-shape curve. The peaks of that curve
+    correspond to the lower position of the press. The number of peaks
+    corresponds to the number of press moves.
+
+    ...
+
+    Attributes
+    ----------
+    x_start : int
+        Horizonal position of top-left corner of the main area.
+    y_start : int
+        Vertical position of top-left corner of the main area.
+    x_end : int
+        Horizonal position of bottom-right corner of the main area.
+    y_end : int
+        Vertical position of bottom-right corner of the main area.
+    x_bar_start : int
+        Horizontal position of the top-left corner of the inner area. This
+        position is relative to the main area.
+    y_bar_start : int
+        Vertical position of the top-left corner of the inner area. This
+        position is relative to the main area.
+    bbox : (int, int, int, int)
+        Inner area define by the position of the top-left corner, width and
+        height.
+    y_pos_history : list
+        Positions of the top-left corner of the inner area.
+    tracker : cv2.Tracker
+        Tracker that finds the position of the inner area.
+    peaks : list
+        Indices of the frames where the inner area is in the bottom position.
+
+    Methods
+    -------
+    init_tracker(frame)
+        Initializes the tracker.
+    process_frame(frame)
+        Processes the frame to update the inner area.
+    calculate_press_down_positions()
+        Calculates the frames where the inner area is in the bottom position.
+    draw_inner_area(frame)
+        Draws the inner area.
+    """
+
+    def __init__(self,
+                    x_center, half_width,
+                    y_center, half_height,
+                    y_bar_start, half_bar_width, half_bar_height,
+                    tracker_type='BOOSTING'):
+        """
+        Parameters
+        ----------
+        x_center : int
+            Horizontal position of the center of the main area.
+        half_width : int
+            Half the width of the main area.
+        y_center : int
+            Vertical position of the center of the main area.
+        half_height : int
+            Half the height of the main area.
+        y_bar_start : int
+            Vertical position of the inner area. This value is relative to the
+            main area.
+        half_bar_width : int
+            Half the width of the inner area.
+        half_bar_height : int
+            Half the height of the inner area.
+        tracker_type : str
+            Name of one of the built-in trackers in OpenCV.
+        """
+
+        # Top-left corner of the main area.
+        self.x_start = int(x_center - half_width)
+        self.y_start = int(y_center - half_height)
+
+        # Bottom-right corner of the main area.
+        self.x_end = int(x_center + half_width)
+        self.y_end = int(y_center + half_height)
+
+        # Top-left corner of the inner area. This position is relative to the
+        # main area.
+        self.y_bar_start = y_bar_start
+        self.x_bar_start = int(half_width - half_bar_width)
+
+        # Inner area
+        self.bbox = (self.x_bar_start, self.y_bar_start,
+                half_bar_width * 2, half_bar_height * 2)
+
+        # Vertical positions of the top-left corner of the inner area
+        self.y_pos_history = None
+
+        # Indices of the frames where the inner area is in the bottom position.
+        self.peaks = None
+
+         # Tracker
+        (major_ver, minor_ver, subminor_ver) = (cv.__version__).split('.')
+        if int(minor_ver) < 3:
+            self.tracker = cv.Tracker_create(tracker_type)
+        else:
+            if tracker_type == 'BOOSTING':
+                self.tracker = cv.TrackerBoosting_create()
+            if tracker_type == 'MIL':
+                self.tracker = cv.TrackerMIL_create()
+            if tracker_type == 'KCF':
+                self.tracker = cv.TrackerKCF_create()
+            if tracker_type == 'TLD':
+                self.tracker = cv.TrackerTLD_create()
+            if tracker_type == 'MEDIANFLOW':
+                self.tracker = cv.TrackerMedianFlow_create()
+            if tracker_type == 'GOTURN':
+                self.tracker = cv.TrackerGOTURN_create()
+            if tracker_type == 'MOSSE':
+                self.tracker = cv.TrackerMOSSE_create()
+            if tracker_type == "CSRT":
+                self.tracker = cv.TrackerCSRT_create()
+
+    def init_tracker(self, frame):
+        """
+        Initializes the tracker. The object to be tracked is defined by the
+        content of the inner area in the frame
+
+        Parameters
+        ----------
+        frame : ndarray
+            3-channel image.
+        """
+
+        # Extract the main area.
+        main_area = self.__extract_main_area(frame)
+
+        # Initialize the tracker with the main and inner areas.
+        self.tracker.init(main_area, self.bbox)
+
+        # Init y value of top-left corner of inner area.
+        self.y_pos_history = np.array(int(self.bbox[1]))
+
+    def process_frame(self, frame):
+        """
+        Processes the frame to update the inner area, that is, finding the
+        position of the object defined in the initial frame.
+
+        Parameters
+        ----------
+        frame : ndarray
+            3-channel image.
+        """
+
+        if self.y_pos_history is None:
+            raise ValueError('Tracker is not initialized')
+
+        main_area = self.__extract_main_area(frame)
+        ok, bbox = self.tracker.update(main_area)
+
+        if ok:
+            self.bbox = bbox
+            # Vertical position of the tracking object in the current frame.
+            self.y_pos_history = np.hstack((self.y_pos_history, int(self.bbox[1])))
+
+    def calculate_press_down_positions(self):
+        """
+        Calculates the frames where the inner area is in the bottom position.
+        The result is set to the peaks attribute.
+        """
+
+        if self.y_pos_history is None:
+            raise ValueError('Tracker is not initialized')
+
+        # Normalize the vertical positions of the top-left corner of the
+        # bounding box of the tracking object.
+        min_ = np.min(self.y_pos_history)
+        max_ = np.max(self.y_pos_history)
+
+        self.y_pos_history = self.y_pos_history - min_
+        self.y_pos_history = self.y_pos_history / (max_ - min_)
+
+        # Find the peaks of the sine-shape curve.
+        peaks, _ = find_peaks(self.y_pos_history, distance=15)
+
+        # Filter the peaks to get just those corresponding to the press in the
+        # down position.
+        valid_peaks = np.argwhere(self.y_pos_history[peaks] > 0.5)
+        self.peaks = peaks[valid_peaks.ravel()]
+
+    def draw_inner_area(self, frame):
+        """
+        Draws the inner area (bounding box of the tracking object).
+
+        Parameters
+        ----------
+        frame : ndarray
+            3-channel image where the inner area will be drawn.
+        """
+
+        main_area = self.__extract_main_area(frame)
+        x, y = int(self.bbox[0]), int(self.bbox[1])
+        w, h = int(self.bbox[2]), int(self.bbox[3])
+
+        H = main_area.shape[0]
+        W = main_area.shape[1]
+        p1 = (x, y)
+        p2 = (x + w, y + h)
+        cv.rectangle(main_area, p1, p2, (255,0,0), 1, 1)
+
+        # Resize for better visualization
+        main_area = cv.resize(main_area, (W * 3, H * 3))
+
+        cv.imshow("Tracking", main_area)
+
+    def __extract_main_area(self, frame):
+        """
+        Extracts the main area from a frame
+
+        Parameters
+        ----------
+        frame : ndarray
+            3-channel image.
+        """
+
+        main_area = frame[self.y_start:self.y_end, self.x_start:self.x_end, :]
+
+        return main_area
