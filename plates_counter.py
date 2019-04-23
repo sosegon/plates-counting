ae495f68b0cc1471f73417b530af4033efec356b
Sebastian <anse23@hotmail.com>
2019-04-23 13:12:58 -0500

    doc: Added comments for documentation
    The comments were added to utils and PlatesCounter.

--- plates_counter.py
+++ plates_counter.py
@@ -3,8 +3,52 @@ import numpy as np
 from utils import extract_area, find_peaks, normalize
 
 class PlatesCounter:
+    """
+    A class to count the plates coming out a shoot in a manufacturing machine.
 
+    The counting is done by measuring the lightness in the area of the shoot for
+    every frame. The result is a 1-D signal. The peaks of that curve correspond
+    to a plate coming out of the shoot.
+
+    ...
+
+    Attributes
+    ----------
+    x_start : int
+        Horizontal position of the top-left corner of the shoot area.
+    y_start : int
+        Vertical position of the top-left corner of the shoot area.
+    width : int
+        Width of the shoot area.
+    height : int
+        Height of the shoot area.
+    light_history : ndarray
+        Array of average lightness of the shoot area for every frame.
+    peaks : ndarray
+        Array of positions for peaks of the average lightness.
+
+    Methods
+    -------
+    init(frame)
+        Initializes the average lightness array.
+    process_frame(frame)
+        Processes a frame to add its average lightness to history.
+    calculate_plates()
+        Calculates the frames where the average lightness is high.
+    """
     def __init__(self, x_start, y_start, width, height):
+        """
+        Parameters
+        ----------
+        x_start : int
+            Horizontal position of the top-left corner of the shoot area.
+        y_start : int
+            Vertical position of the top-left corner of the shoot area.
+        width : int
+            Width of the shoot area.
+        height : int
+            Height of the shoot area.
+        """
         self.x_start = x_start
         self.y_start = y_start
         self.width = width
@@ -13,15 +57,35 @@ class PlatesCounter:
         self.peaks = None
 
     def init(self, frame):
+        """
+        Initializes the average lightness array.
+
+        Parameters
+        ----------
+        frame : ndarray
+            3-channel image
+        """
+
+        # Extract the shoot area.
         area = extract_area(frame, self.x_start, self.y_start,
             self.width, self.height)
 
+        # Change to color space to get the lightness.
         area = cv.cvtColor(area, cv.COLOR_BGR2HLS)
 
+        # Get the average lightness and start the history array.
         light_avg = np.mean(area[:, :, 1])
         self.light_history = np.array(light_avg)
 
     def process_frame(self, frame):
+        """
+        Processes a frame to add its average lightness to history.
+
+        Parameters
+        ----------
+        frame : ndarray
+            3-channel image.
+        """
         if self.light_history is None:
             raise ValueError('Counter is not initialized')
 
@@ -34,6 +98,10 @@ class PlatesCounter:
         self.light_history = np.hstack((self.light_history, light_avg))
 
     def calculate_plates(self):
+        """
+        Calculates the frames where the average lightness is high.
+        The result is set to the peaks attribute.
+        """
         if self.light_history is None:
             raise ValueError('Counter is not initialized')
 
--- utils.py
+++ utils.py
@@ -2,11 +2,44 @@ import numpy as np
 from scipy.signal import find_peaks as fp
 
 def extract_area(frame, x, y, width, height):
+    """
+    Extracts an area from a frame.
 
+    Parameters
+    ----------
+    frame : ndarray
+        3-channel image.
+    x : int
+        Horizonal position of top-left corner of the area.
+    y : int
+        Vertical position of top-left corner of the area.
+    width : int
+        Width of the area.
+    height : int
+        Height of the area.
+
+    Returns
+    -------
+    ndarray : 3-channel image.
+    """
     return frame[y:y + height, x:x+width, :]
 
-
 def find_peaks(points, threshold=0.5):
+    """
+    Finds the peaks in a signal.
+
+    Parameters
+    ----------
+    points : ndarray
+        1-D array of normalized data points.
+    threshold : float
+        Limit to filter peaks; those below that value are discarded.
+
+    Returns
+    -------
+    ndarray : 1-D array with the indices corresponding to the peaks of
+        points.
+    """
 
     # Find the peaks
     peaks, _ = fp(points, distance=15)
@@ -17,7 +50,20 @@ def find_peaks(points, threshold=0.5):
     return peaks[valid_peaks.ravel()]
 
 def normalize(points, min_, max_):
-    points = points - min_
-    points = points / (max_ - min_)
+    """`
+    Normalizes the points of a signal.
 
-    return points
+    Parameters
+    ----------
+    points : ndarray
+        1-D array of data points
+    min_ : int
+        Low limit of the range to normalize the points.
+    max_ : int
+        High limit of the range to normalize the points.
+
+    Returns
+    -------
+    ndarray : 1-D array of normalized points.
+    """
+    return points - min_ / (max_ - min_)
